388850bcb6aee5c9e7c043fde7a6466497fadb9a
Sonia <149040956+desamsonia@users.noreply.github.com>
2024-07-16 16:58:20 +0530

    del_word_if_chars_repeated_method1

---
+++ chars.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<string.h>
+
+int rep_char(char str[])
+{
+    int i,j,k;
+    j=strlen(str);
+    for(i=0;i<j;i++)
+    {
+            for(k=i+1;k<j;k++)
+            {
+                    if(str[i] == str[k])
+                            return 1;
+            }
+    }
+    return 0;
+}
+
+void sep(char *msg)
+{
+        char str[100];
+        char s[7][10];
+        strcpy(str,msg);
+        char *p=str;
+        char *q;
+        p=strtok(msg," ");
+        int i=0;
+        while(p)
+        {
+                q=p;
+                strcpy(s[i++],p);
+                p=strtok(NULL," ");
+                *(q+strlen(q))=' ';
+	}
+	char res[100];
+        int n =i,r;
+	for(i=0;i<n;i++)
+		printf("%s ",s[i]);
+	for(i=0;i<n;i++)
+	{
+	        r = rep_char(s[i]);
+		if(!r)
+		{
+	                if(i==0)
+			{
+			 strcpy(res,s[i]);
+                         strcpy(res+strlen(res) , " ");
+			 printf("res:%s\n",res);
+			}
+
+			else 
+			{
+				strcat(res+strlen(res),s[i]);
+				strcpy(res+strlen(res)," ");
+				printf("res:%s\n",res);
+			}
+		}
+	}
+	printf("res:%s\n",res);
+}
+
+int main()
+{
+        char str[100];
+        printf("Enter string:");
+        gets(str);
+        sep(str);
+}
