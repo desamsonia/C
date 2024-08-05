86520535e75e26b564d7f990f1330272c5cbf56a
Sonia <149040956+desamsonia@users.noreply.github.com>
2024-08-05 23:26:06 +0530

    mcqs

---
+++ pascal.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+	int i=10;
+	void pascal f(int,int,int);
+	f(i++, i++, i++);
+	printf(" %d",i);
+	return 0;
+}
+void pascal f(integer :i,integer:j,integer :k)
+{
+write(i,j,k);
+}
+
